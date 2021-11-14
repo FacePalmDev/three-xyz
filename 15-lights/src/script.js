47a7bc9232708d1d3cd72fbd13d8acf5a44232fe
Kiran Randhawa <4026553+FacePalmDev@users.noreply.github.com>
2021-11-14 13:25:03 +0000

    Added ambient light intenisty to GUI slider

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -21,6 +21,8 @@ const scene = new THREE.Scene()
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
+gui.add(ambientLight, 'intensity', 0, 1);
+
 /**
  * Objects
  */
